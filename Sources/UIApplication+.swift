df6f9d0da767d2c3557e095e90623c578c9de532
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-12-15 01:46:05 +0900

    UIApplication+

---
+++ Sources/UIApplication+.swift
@@ -0,0 +1,25 @@
+//
+//  UIApplication+.swift
+//  SwiftExtensions
+//
+//  Created by Tatsuya Tanaka on 20171215.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    public var topViewController: UIViewController? {
+        guard var topViewController = UIApplication.shared.keyWindow?.rootViewController else { return nil }
+
+        while let presentedViewController = topViewController.presentedViewController {
+            topViewController = presentedViewController
+        }
+        return topViewController
+    }
+
+
+    public var topNavigationController: UINavigationController? {
+        return topViewController as? UINavigationController
+    }
+}
