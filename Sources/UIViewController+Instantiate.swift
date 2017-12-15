d7f5285321c5ff9451f915bc319079ed81ec5648
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-12-16 02:51:22 +0900

    Add StoryboardInstantiatable

---
+++ Sources/UIViewController+Instantiate.swift
@@ -0,0 +1,49 @@
+//
+//  UIViewController+Instantiate.swift
+//  SwiftExtensions
+//
+//  Created by Tatsuya Tanaka on 20171215.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+import UIKit
+
+public enum StoryboardInstantiateType {
+    case initial
+    case identifier(String)
+}
+
+public protocol StoryboardInstantiatable {
+    static var storyboardName: String { get }
+    static var storyboardBundle: Bundle { get }
+    static var instantiateType: StoryboardInstantiateType { get }
+}
+
+extension StoryboardInstantiatable where Self: NSObjectProtocol {
+    public static var storyboardName: String {
+        return className
+    }
+
+    public static var storyboardBundle: Bundle {
+        return Bundle(for: self)
+    }
+
+    fileprivate static var storyboard: UIStoryboard {
+        return UIStoryboard(name: storyboardName, bundle: storyboardBundle)
+    }
+
+    public static var instantiateType: StoryboardInstantiateType {
+        return .identifier(className)
+    }
+}
+
+extension StoryboardInstantiatable where Self: UIViewController {
+    public static func instantiate() -> Self {
+        switch instantiateType {
+        case .initial:
+            return storyboard.instantiateInitialViewController() as! Self
+        case .identifier(let identifier):
+            return storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+        }
+    }
+}
---
+++ Tests/UIViewController+InstantiateTests.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewController+InstantiateTests.swift
+//  SwiftExtensionsTests
+//
+//  Created by Tatsuya Tanaka on 20171215.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+import XCTest
+import SwiftExtensions
+
+final class InitialViewController: UIViewController, StoryboardInstantiatable {
+    @IBOutlet weak var label: UILabel!
+    static var instantiateType: StoryboardInstantiateType {
+        return .initial
+    }
+}
+
+final class IdentifierViewController: UIViewController, StoryboardInstantiatable {
+    @IBOutlet weak var label: UILabel!
+    static var storyboardName: String { return InitialViewController.className }
+}
+
+class UIViewController_InstantiateTests: XCTestCase {
+    
+    func testInstantiateByInitial() {
+        let viewControlelr = InitialViewController.instantiate()
+        XCTAssertNil(viewControlelr.label)
+        _ = viewControlelr.view
+        XCTAssertNotNil(viewControlelr.label)
+    }
+
+    func testInstantiateByIdentifier() {
+        let viewControlelr = IdentifierViewController.instantiate()
+        XCTAssertNil(viewControlelr.label)
+        _ = viewControlelr.view
+        XCTAssertNotNil(viewControlelr.label)
+    }
+}
