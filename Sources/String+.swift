8782b059f91d54193a851a56d4ca4011533a4c9c
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-12-18 00:57:54 +0900

    Add string width transformer

--- Sources/String+.swift
+++ Sources/String+.swift
@@ -52,3 +52,19 @@ public extension String {
         return String(self[start..<endIndex])
     }
 }
+
+public extension String {
+    public var halfWidth: String {
+        return transformFullWidthToHalfWidth(reverse: false)
+    }
+
+    public var fullWidth: String {
+        return transformFullWidthToHalfWidth(reverse: true)
+    }
+
+    private func transformFullWidthToHalfWidth(reverse: Bool) -> String {
+        let string = NSMutableString(string: self) as CFMutableString
+        CFStringTransform(string, nil, kCFStringTransformFullwidthHalfwidth, reverse)
+        return string as String
+    }
+}
--- Tests/StringTests.swift
+++ Tests/StringTests.swift
@@ -42,3 +42,89 @@ extension StringTests {
         XCTAssertEqual(string[4...], "456789")
     }
 }
+
+extension StringTests {
+    func testHalfWidthFullNumberToHalf() {
+        let target = "１２３"
+        let expected = "123"
+        XCTAssertEqual(expected, target.halfWidth)
+    }
+
+    func testHalfWidthHalfNumberToHalf() {
+        let target = "12345"
+        let expected = "12345"
+        XCTAssertEqual(expected, target.halfWidth)
+    }
+
+    func testHalfWidthFullAlphabetToHalf() {
+        let target = "ＡＢＣＤ"
+        let expected = "ABCD"
+        XCTAssertEqual(expected, target.halfWidth)
+    }
+
+    func testHalfWidthHalfAlphabetToHalf() {
+        let target = "ABCD"
+        let answer = "ABCD"
+        XCTAssertEqual(answer, target.halfWidth, "半角英字が半角英字になることを確認")
+    }
+
+    func testHalfWidthFullAlphabetAndNumberToHalf() {
+        let target = "１２ＡＢ"
+        let expected = "12AB"
+        XCTAssertEqual(expected, target.halfWidth)
+    }
+
+    func testHalfWidthHalfOrFullAlphabetAndNumberToHalf() {
+        let target = "１２3ＡＢcde"
+        let expected = "123ABcde"
+        XCTAssertEqual(expected, target.halfWidth)
+    }
+
+    func testHalfWidthひらがなToひらがな() {
+        let target = "あいうえお"
+        let expected = "あいうえお"
+        XCTAssertEqual(expected, target.halfWidth)
+    }
+
+    func testFullWidthHalfNumberToFull() {
+        let target = "123"
+        let expected = "１２３"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+
+    func testFullWidthFullNumberToFull() {
+        let target = "１２３４５"
+        let expected = "１２３４５"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+
+    func testFullWidthHalfAlphabetToFull() {
+        let target = "ABCD"
+        let expected = "ＡＢＣＤ"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+
+    func testFullWidthFullAlphabetToFull() {
+        let target = "ＡＢＣＤ"
+        let expected = "ＡＢＣＤ"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+
+    func testFullWidthHalfAlphabetAndNumberToFull() {
+        let target = "12AB"
+        let expected = "１２ＡＢ"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+
+    func testFullWidthFullOrHalfAlphabetAndNumberToFull() {
+        let target = "１２3ABcde"
+        let expected = "１２３ＡＢｃｄｅ"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+
+    func testFullWidthひらがなToひらがな() {
+        let target = "あいうえお"
+        let expected = "あいうえお"
+        XCTAssertEqual(expected, target.fullWidth)
+    }
+}
