25998cb8ab2cad7227796aae516b1a2a48c13310
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-12-17 21:53:02 +0900

    Add UIView+

---
+++ Sources/UIView+.swift
@@ -0,0 +1,25 @@
+//
+//  UIView+.swift
+//  SwiftExtensions
+//
+//  Created by Tatsuya Tanaka on 20171217.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    public func fillSuperview() {
+        guard let superview = self.superview else { return }
+        translatesAutoresizingMaskIntoConstraints = superview.translatesAutoresizingMaskIntoConstraints
+        if translatesAutoresizingMaskIntoConstraints {
+            autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            frame = superview.bounds
+        } else {
+            topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+            leftAnchor.constraint(equalTo: superview.leftAnchor).isActive = true
+            rightAnchor.constraint(equalTo: superview.rightAnchor).isActive = true
+        }
+    }
+}
---
+++ Tests/UIViewTests.swift
@@ -0,0 +1,41 @@
+//
+//  UIViewTests.swift
+//  SwiftExtensionsTests
+//
+//  Created by Tatsuya Tanaka on 20171217.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+import XCTest
+import SwiftExtensions
+
+class UIViewTests: XCTestCase {
+
+    func testFillSuperviewByAutoresizingMask() {
+        let frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        let superview = UIView(frame: .init(x: 0, y: 0, width: 400, height: 400))
+        let view = UIView(frame: frame)
+        view.fillSuperview()
+        XCTAssertEqual(view.frame, frame)
+        superview.addSubview(view)
+        view.fillSuperview()
+        XCTAssertEqual(view.frame, superview.bounds)
+    }
+
+    func testFillSuperviewByAutoLayout() {
+        let frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        let superview = UIView(frame: .init(x: 0, y: 0, width: 400, height: 400))
+        superview.translatesAutoresizingMaskIntoConstraints = false
+        superview.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        superview.heightAnchor.constraint(equalToConstant: 400).isActive = true
+
+        let view = UIView(frame: frame)
+        view.fillSuperview()
+        XCTAssertEqual(view.frame, frame)
+        superview.addSubview(view)
+        view.fillSuperview()
+        XCTAssertEqual(view.frame, frame)
+        superview.layoutIfNeeded()
+        XCTAssertEqual(view.frame, superview.bounds)
+    }
+}
