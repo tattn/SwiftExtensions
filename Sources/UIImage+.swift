56fd93abbd5b89a91b70bb84634b9378e9f87d1c
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-12-18 00:30:00 +0900

    Add UIImage extensions

--- Sources/UIImage+.swift
+++ Sources/UIImage+.swift
@@ -31,4 +31,23 @@ public extension UIImage {
             self.init()
         }
     }
+
+    func tint(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+
+        guard let context: CGContext = UIGraphicsGetCurrentContext(), let cgImage = cgImage else {
+            return UIImage()
+        }
+
+        context.clip(to: rect, mask: cgImage)
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else {
+            return UIImage()
+        }
+        UIGraphicsEndImageContext()
+
+        return image
+    }
 }
