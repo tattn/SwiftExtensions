d57eeaff86cf6c7ac19b72d35bd31ee9a670e020
Happy HumanPointer <45888860+SafelySwift@users.noreply.github.com>
2019-01-07 11:17:07 -0500

    Update Operator+.swift

--- Sources/Operator+.swift
+++ Sources/Operator+.swift
@@ -25,15 +25,15 @@ precedencegroup ForwardPipe {
   higherThan: LogicalConjunctionPrecedence
 }
 
-infix operator |=: ForwardPipe
+infix operator |>: ForwardPipe
 
 /// Piping allows east workflow of modifying an object
 ///
-/// let result = value |= { $0 * 2 } |= { $0 * $0 }
+/// let result = value |> { $0 * 2 } |> { $0 * $0 }
 /// is the same as
 /// let result = { (x: Int) -> Int in return (x * 2) * (x * 2 }.(value)
 ///
 /// Because the pipe operator is left associative, you can add as many closures as you want  
-public func |= <T, U>(lhs: T, rhs: (T) throws -> U) rethrows -> U {
+public func |> <T, U>(lhs: T, rhs: (T) throws -> U) rethrows -> U {
   return return try rhs(lhs)
 }
