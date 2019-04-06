510c1f3ee316e1af6f245991e35354bd7e42d4fa
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2019-04-06 16:35:16 +0900

    Add Result+

---
+++ Sources/Result+.swift
@@ -0,0 +1,21 @@
+//
+//  Result+.swift
+//  SwiftExtensions
+//
+//  Created by Tatsuya Tanaka on 2019/04/06.
+//  Copyright © 2019 tattn. All rights reserved.
+//
+
+import Foundation
+
+public extension Result {
+    /// Returns the failure value
+    ///
+    /// - Returns: The failure value, if the instance represents a failure. Otherwise, nil.
+    func error() -> Failure? {
+        switch self {
+        case .success: return nil
+        case .failure(let error): return error
+        }
+    }
+}
---
+++ Tests/ResultTests.swift
@@ -0,0 +1,20 @@
+//
+//  ResultTests.swift
+//  SwiftExtensionsTests
+//
+//  Created by Tatsuya Tanaka on 2019/04/06.
+//  Copyright © 2019 tattn. All rights reserved.
+//
+
+import XCTest
+
+class ResultTests: XCTestCase {
+    func testError() {
+        struct MyError: Error, Equatable {
+            let message: String
+        }
+        let error = MyError(message: "MyError")
+        let result = Result<Int, MyError>.failure(error)
+        XCTAssertEqual(result.error(), error)
+    }
+}
