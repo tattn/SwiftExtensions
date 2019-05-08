d646ef901b57636b2f7de6553f8731dd9f483b34
nathan <453816118@qq.com>
2019-05-08 17:53:00 +0800

    add array extension

--- Sources/Array+.swift
+++ Sources/Array+.swift
@@ -20,7 +20,70 @@ public extension Array where Element: Equatable {
     mutating func remove(_ elements: [Element]) -> [Index] {
         return elements.compactMap { remove($0) }
     }
+    
+    
+    @discardableResult
+    mutating func append(_ newArray: Array) -> CountableRange<Int> {
+        let range = count..<(count + newArray.count)
+        self += newArray
+        return range
+    }
+    
+    @discardableResult
+    mutating func insert(_ newArray: Array, at index: Int) -> CountableRange<Int> {
+        let mIndex = Swift.max(0, index)
+        let start = Swift.min(count, mIndex)
+        let end = start + newArray.count
+        
+        let left = self[0..<start]
+        let right = self[start..<count]
+        self = left + newArray + right
+        return start..<end
+    }
+    
+    func each(_ exe: (Int, Element) -> ()) {
+        for (index, item) in enumerated() {
+            exe(index, item)
+        }
+    }
 }
+
+public extension Array {
+    
+    /// Random item from array.
+    var randomItem: Element? {
+        if self.isEmpty { return nil }
+        let index = Int(arc4random_uniform(UInt32(count)))
+        return self[index]
+    }
+    
+    /// Shuffled version of array.
+    var shuffled: [Element] {
+        var arr = self
+        for _ in 0..<10 {
+            arr.sort { (_,_) in arc4random() < arc4random() }
+        }
+        return arr
+    }
+    
+    /// Shuffle array.
+    mutating func shuffle() {
+        // https://gist.github.com/ijoshsmith/5e3c7d8c2099a3fe8dc3
+        for _ in 0..<10 {
+            sort { (_,_) in arc4random() < arc4random() }
+        }
+    }
+    
+    /// Element at the given index if it exists.
+    ///
+    /// - Parameter index: index of element.
+    /// - Returns: optional element (if exists).
+    func item(index:Int) -> Element? {
+        guard index >= 0 && index < count else { return nil }
+        return self[index]
+    }
+}
+
 
 public extension Array where Element: Hashable {
     mutating func unify() {
