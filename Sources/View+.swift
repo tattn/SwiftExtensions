0f684bec774bf331dc72a6a7f14d6e063aa4025e
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2019-07-03 01:20:25 +0900

    Add View+.swift

---
+++ Sources/View+.swift
@@ -0,0 +1,23 @@
+//
+//  View+.swift
+//  SwiftExtensions
+//
+//  Created by Tatsuya Tanaka on 2019/07/03.
+//  Copyright © 2019 tattn. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13, *)
+public extension View {
+    func wrappedInUIViewController() -> UIHostingController<Self> {
+        UIHostingController(rootView: self)
+    }
+
+    func wrappedInUIView(parent: UIViewController) -> UIView {
+        let hostingController = wrappedInUIViewController()
+        parent.addChild(hostingController)
+        hostingController.didMove(toParent: parent)
+        return hostingController.view
+    }
+}
