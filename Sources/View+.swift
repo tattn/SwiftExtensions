077a6c28943f843c3c87873f93bc4fe11277075d
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2019-11-25 00:41:52 +0900

    Add extensions for View

--- Sources/View+.swift
+++ Sources/View+.swift
@@ -28,5 +28,33 @@ public extension View {
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension View {
-    var erased: AnyView { AnyView(self) }
+    @inlinable var erased: AnyView { AnyView(self) }
+
+    @inlinable func hidden(isHidden: Bool) -> Self? {
+        isHidden ? nil : self
+    }
+
+    func onChangeSize(perform: @escaping (CGSize) -> Void) -> some View {
+        self.background(SizePreferenceReader())
+            .onPreferenceChange(SizePreferenceKey.self, perform: perform)
+    }
+}
+
+private struct SizePreferenceKey: PreferenceKey {
+    static let defaultValue: CGSize = .zero
+
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
+    }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+private struct SizePreferenceReader: View {
+    var body: some View {
+        GeometryReader { geometry in
+            return Rectangle()
+                .foregroundColor(.clear)
+                .preference(key: SizePreferenceKey.self, value: geometry.size)
+        }
+    }
 }
